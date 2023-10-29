01d7a3cf16b4f6e51e4bc018ac213905ab45cd8f
Fede1771 <federicoburgos98@gmail.com>
2023-10-29 15:56:22 -0300

    modificacion preentrega1

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
-import logo from './logo.svg';
+import React from 'react';
+import Navbar from './components/NavBar';
+import ItemListContainer from './components/ItemListContainer';
 import './App.css';
 
 function App() {
-  return (
+   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Navbar />
+        <ItemListContainer texto="Bienvenido" /> {/* Renderiza ItemListContainer debajo de Navbar */}
       </header>
     </div>
   );
